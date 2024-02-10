4a1a015eff224ce526f462deeecd408f11f2550f
gabe <gabeklav@fastmail.com>
2024-02-10 16:40:55 -0500

    format

--- process-chs.py
+++ process-chs.py
@@ -27,10 +27,9 @@ for chapter in chapters:
     chapter_files = list(filter(lambda file_name: chapter in file_name, file_names))
     chapter_files.sort()
 
-    # chapter_file_name = " - ".join(os.path.splitext(chapter_files[0])[0].split(" - ")[:3])
     chapter_file_name = re.sub(r" - p\d{3}", "", chapter_files[0])
     with zipfile.ZipFile(f"{chapter_file_name}.zip", "w") as ch_zip:
         for chapter_file in chapter_files:
             ch_zip.write(os.path.join(directory_path, chapter_file))
-    
+
     os.rename(f"{chapter_file_name}.zip", f"{chapter_file_name}.cbz")
